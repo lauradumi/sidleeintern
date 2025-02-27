22e2a6b14ea6fe5d66660644746b0e7ff1ccf3b1
Laura Dumitriu <154947819+lauradumi@users.noreply.github.com>
2025-02-27 09:54:24 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+const questions = [
+  {
+    numb: 1,
+    question: "Code en musique ou en silence?",
+    answer: "Bruit de fond léger ☕",
+    options: [
+      "Musique à fond 🎶",
+      "Silence total 🤫",
+      "Bruit de fond léger ☕",
+      "Ça dépend du projet 🎯"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Quand une fonctionnalité marche du premier coup, je…",
+    answer: "Rafraîchis trois fois pour être sûre 😅",
+    options: [
+      "Rafraîchis trois fois pour être sûre 😅",
+      "Fête ça avec un café ☕",
+      "Ne fais pas confiance, y’a forcément un bug caché 🐛",
+      "Screenshot direct, ça n’arrivera plus jamais 📸"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Ton éditeur de code favori ?",
+    answer: "VS Code",
+    options: [
+      "SublimeText",
+      "Notepad++",
+      "VS Code",
+      "Phoenix Code"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Un bug critique en prod, je…",
+    answer: "Appelle un collègue à la rescousse 📞 ",
+    options: [
+      "Prends une grande inspiration et fonces 🧘‍♂️",
+      "Appelle un collègue à la rescousse 📞 ",
+      "Blâmes discrètement Git 🙃 ",
+      "Dis “ça marchait chez moi” et pleure en silence 😭"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Si tu étais un langage de programmation, tu serais…",
+    answer: "JavaScript – Flexible et toujours en mouvement",
+    options: [
+      "JavaScript – Flexible et toujours en mouvement",
+      "Python – Clair, efficace et polyvalent",
+      "Ruby – Élégant, fluide et orienté humain",
+      "C++ – Puissant et structuré"
+    ]
+  },
+];
